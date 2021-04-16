9b13af39bd23a72af0cd9eac01761f5367ca38db
Anderson Olliver <pla.olliver@gmail.com>
2021-04-15 22:49:53 -0400

    adiciona estilo a splash screen

--- lib/app/core/pages/splash_screen_page.dart
+++ lib/app/core/pages/splash_screen_page.dart
@@ -2,13 +2,13 @@ import 'package:financa_pessoal/app/modules/shared/store/auth_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../const.dart';
+import '../util/const.dart';
 
 class SplashScreenPage extends StatelessWidget {
   SplashScreenPage() {
     Modular.get<AuthStore>().checkLogin().then((v) async {
       return Future.delayed(
-        Duration(seconds: 2),
+        Duration(seconds: 5),
         () {
           var route = v ? Routes.entries : Routes.login;
           return Modular.to.pushReplacementNamed(route);
@@ -20,8 +20,44 @@ class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: CircularProgressIndicator(),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Color(0xff4c47b5),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white)
+                // backgroundColor: Colors.white,
+                ),
+            SizedBox(
+              height: 28.5,
+            ),
+            Text(
+              "Minhas",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.w300,
+                fontFamily: "Roboto",
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Finanças",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 36,
+                fontWeight: FontWeight.w500,
+                fontFamily: "Roboto",
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/app/core/util/const.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static String home = '/home';
+  static String login = '/login';
+  static String register = '/register';
+  static String entries = '/entries';
+  static String categories = '/categories';
+}
